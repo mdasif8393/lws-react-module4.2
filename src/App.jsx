b5a723b5190d8e86efbf5c2a73e5f9222a0ba71e
mdasif8393 <md.asif.8393@gmail.com>
2024-04-17 21:36:23 +0600

    4.28 - Performance Optimization React.memo() useMemo useCallBack

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import Form from "./components/Form";
-import SaveButton from "./components/SaveButton";
-import StatusBar from "./components/StatusBar";
+import { useCallback, useMemo, useState } from "react";
+import Button from "./components/Button";
+import ShowCount from "./components/ShowCount";
+import Title from "./components/Title";
 
 export default function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementByOne = useCallback(() => {
+    setCount1((prevCount) => prevCount + 1);
+  }, []);
+
+  const incrementByFive = useCallback(() => {
+    setCount2((prevCount) => prevCount + 5);
+  }, []);
+
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) {
+      i++;
+    }
+
+    return count1 % 2 === 0;
+  }, [count1]);
+
   return (
-    <div>
-      <StatusBar />
-      <SaveButton />
-      <br />
-      <br />
-      <Form />
+    <div className="app">
+      <Title />
+      <ShowCount count={count1} title="Count 1" />
+
+      <span>{isEven ? "Even" : "Odd"}</span>
+
+      <Button handleClick={incrementByOne}>Increment By One</Button>
+      <hr />
+      <ShowCount count={count2} title="Count 2" />
+      <Button handleClick={incrementByFive}>Increment By Five</Button>
     </div>
   );
 }
